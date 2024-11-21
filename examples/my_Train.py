afece25cd767ec4a8c3d547108d19f606dfa9528
SajaniJoshi <joshisajani@gmail.com>
2024-11-21 13:22:23 +0100

    train is calulated separately

---
+++ examples/my_Train.py
@@ -0,0 +1,108 @@
+import os
+import csv
+import mxnet as mx
+from mxnet import autograd
+from decode.FracTAL_ResUNet.nn.loss.mtsk_loss import mtsk_loss
+from decode.postprocessing.instance_segmentation import InstSegm
+from myModel import MyFractalResUNetcmtsk, ReduceLROnPlateau, LossModel
+
+
+epochs = 50
+batch_size = 8
+learning_rate = 0.001
+
+num_classes = 2
+ctx = mx.cpu()  
+depth = 6
+nfilters_init = 32
+
+loss_path_plot = r"D:\Source\Test\data\Output\Fractal\Loss\loss_VNIR_plot.png"
+loss_path_csv = r"D:\Source\Test\data\Output\Fractal\Loss\loss_VNIR.csv"
+trained_model_path = r"D:\Source\Test\data\Output\Fractal\Model_VNIR_params"
+
+class myTrain:
+    def __init__(self, images, masks, image_dict, mask_dict, train_ids, val_ids):
+        self.images= images
+        self.masks= masks
+        self.image_dict = image_dict
+        self.mask_dict = mask_dict
+        self.train_ids = train_ids
+        self.val_ids = val_ids
+           
+    def train(self, count=3): #Track epoch losses, backward with multitasking operation
+        netTrain = MyFractalResUNetcmtsk(False, "", ctx, nfilters_init= nfilters_init, depth= depth, num_classes= num_classes)
+        trainer = mx.gluon.trainer.Trainer(netTrain.net.collect_params(), 'adam', {'learning_rate': learning_rate})
+        reduce_lr = ReduceLROnPlateau(trainer, patience=5, factor=0.1) # Example usage inside a training loop
+        myMTSKL = mtsk_loss(depth=0, NClasses=num_classes) # get default Tanimoto loss (depth=0) function for multitasking operation
+        
+        train_losses = []
+        val_losses = []
+        loss_each_epoch = []
+        model_list = []
+
+        print('Start training now')
+        for epoch in range(epochs): # Train for as many num_epochs you want
+            print(f'current epoch: {epoch}')
+            train_loss = 0.0     # compute training loss
+            for id in self.train_ids[:count]: # NOTE: img/mask contain batches of images/lables each with the size (batch_size, H, W)
+                print(id)
+                img = self.images.getImage(id, self.image_dict)
+                mask = self.masks.getImage(id, self.mask_dict)
+                if img is None or mask is None:
+                    continue
+                # forward + backward
+                with autograd.record():
+                    ListOfPredictions = netTrain.net(img)
+                    loss = myMTSKL.loss(ListOfPredictions, mask)
+                loss.backward()
+                trainer.step(1)
+                #using one image so it does not need step trainer.step(1) # update parameters
+                train_loss += loss.mean().asscalar() # calculate training metrics
+            current_epoch_loss = train_loss / len(self.train_ids)  # compute overall loss
+            train_losses.append(current_epoch_loss)    # now append the epoch loss
+    
+            # compute validation loss
+            val_loss = 0.0
+            for id in self.val_ids[:count]:           
+                img = self.images.getImage(id, self.image_dict)
+                mask = self.masks.getImage(id, self.mask_dict)
+                if img is None or mask is None:
+                    continue
+                ListOfPredictions = netTrain.net(img)  # forward only
+                loss = myMTSKL.loss(ListOfPredictions, mask)  # get loss
+                val_loss += loss.mean().asscalar() # calculate validation metrics
+
+            # compute overall loss
+            current_epoch_val_loss = val_loss/ len(self.val_ids)
+            val_losses.append(current_epoch_val_loss)
+            
+            reduce_lr.step(current_epoch_val_loss)  # Adjust learning rate if validation loss stagnates
+    
+        # Track loss and model for analysis    
+        loss_each_epoch.append({"Current Epoch": epoch, "Traing Loss": current_epoch_loss, "Validation loss": current_epoch_val_loss})
+        current_model = LossModel(epoch, current_epoch_loss, current_epoch_val_loss, netTrain.net)
+        model_list.append(current_model)
+        
+        return loss_each_epoch, model_list
+    
+    def saveLosses(self, loss_each_epoch): #Write each epoch loss in CSV
+        headers = ["Current Epoch", "Traing Loss", "Validation loss"]
+        with open(loss_path_csv, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=headers)
+            writer.writeheader()
+            writer.writerows(loss_each_epoch)
+        print(f"Data saved to {loss_path_csv}")
+    
+    def SaveModels(self, model_list):
+        for model in model_list:
+            path =  os.path.join(trained_model_path, f"model_VNIR_{model.epoch}.params")
+            model.net.save_parameters(path)
+    
+    def trainModel(self):
+        loss_each_epoch, model_list = self.train()
+        self.saveLosses(loss_each_epoch)
+        self.SaveModels(model_list)
+        
+        
+        
+        
