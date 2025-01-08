2db6940a723e61dc8288f1dc7ce3d7323f45b6c8
SajaniJoshi <56039237+SajaniJoshi@users.noreply.github.com>
2025-01-08 23:26:22 +0100

    small modification

--- examples/my_Save.py
+++ examples/my_Save.py
@@ -1,15 +1,14 @@
 import os
 import csv  
-import const
 
 def saveAsCSV(headers, path,  data): #Write each epoch loss in CSV
-        with open(path, mode='w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=headers)
-            writer.writeheader()
-            writer.writerows(data)
+    with open(path, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(data)
         print(f"Data saved to {path}")
     
-def SaveModels(model_list):
+def SaveModels(path, model_list):
+    os.makedirs(path, exist_ok=True)
     for model in model_list:
-        path =  os.path.join(const.trained_model_path, f"model_VNIR_{model.epoch}.params")
-        model.net.save_parameters(path)
+        model.net.save_parameters(os.path.join(path, f"model_VNIR_{model.epoch}.params"))
