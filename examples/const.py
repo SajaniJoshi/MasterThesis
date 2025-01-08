665e5cf5d0f4d43a8c84a0fa4cc0271851348339
SajaniJoshi <56039237+SajaniJoshi@users.noreply.github.com>
2025-01-08 23:31:18 +0100

    Removed redundant paths

--- examples/const.py
+++ examples/const.py
@@ -2,15 +2,3 @@ images_2022 =r"D:\Source\Input\Data\2022\BB\08X_Features_Multi"
 masks_2022 = r"D:\Source\Input\Data\2022\BB\XX_Reference_Masks_ResUNetA"
 images_2010 = r'D:\Source\Input\Data\2010\BB\08X_Features_Multi'
 
-
-predicted_2010_VNIR = r'D:\Source\Output\Result_2010\VNIR'
-result_2022_VNIR = r'D:\Source\Output\Result_2022\VNIR'
-result_2010_VNIR = r'D:\Source\Output\Result_2010\VNIR'
-
-iou_2022 = r"D:\Source\Output\Loss\IOU_2022.csv"
-iou_2010 = r"D:\Source\Output\Loss\IOU_2010.csv"
-
-loss_path_plot = r"D:\Source\Output\Loss\loss_VNIR_plot.png"
-loss_path_csv = r"D:\Source\Output\Loss\loss_VNIR.csv"
-trained_model_VNIR_path = r"D:\Source\Output\Models\Model_VNIR_params"
-model_VNIR_params =r"D:\Source\Output\Models\Model_VNIR_params\model_VNIR_49.params"
